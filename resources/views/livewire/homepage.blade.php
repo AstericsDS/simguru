32c2bb8d0b459402f9783227c689baf5821d3e7b
Acokii <zaffaz1106@gmail.com>
2025-04-15 14:04:43 +0700

    hover card

--- resources/views/livewire/homepage.blade.php
+++ resources/views/livewire/homepage.blade.php
@@ -1,63 +1,43 @@
 <div class="py-5 px-10">
   <div class="breadcrumbs text-sm bg-[#006569] rounded-box p-5">
     <ul>
-      <li class="font-bold text-3xl"><a>Kampus</a></li>
+      <li class="font-bold text-3xl"><a href="/">Kampus</a></li>
     </ul>
   </div>
   <div class="grid grid-cols-2 gap-3 mt-3">
-    <div class="card bg-base-100 image-full shadow-sm hover:bg-base-200">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Card Title</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+    <div class="card group bg-[url(/public/backgrounds/homepage.png)] bg-cover bg-gray-500 h-90">
+      <div class="card-body rounded-lg group-hover:backdrop-blur-xs transition-all duration-400">
+        <h2 class="card-title text-7xl group-hover:text-5xl transition-all duration-400">Kampus A</h2>
+        <p class="hidden group-hover:text-2xl group-hover:inline transition-all duration-400">Kampus utama UNJ</p>
+        <p class="hidden group-hover:text-lg group-hover:inline transition-all duration-400">Jl. R.Mangun Muka Raya, RT.11/RW.14, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</p>
+        <div class="card-actions justify-end hover">
         </div>
       </div>
     </div>
-    <div class="card bg-base-100 image-full shadow-sm">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Card Title</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+    <div class="card group bg-[url(/public/backgrounds/homepage.png)] bg-cover bg-gray-500 h-90">
+      <div class="card-body rounded-lg group-hover:backdrop-blur-xs transition-all duration-400">
+        <h2 class="card-title text-7xl group-hover:text-5xl transition-all duration-400">Kampus A</h2>
+        <p class="hidden group-hover:text-2xl group-hover:inline transition-all duration-400">Kampus utama UNJ</p>
+        <p class="hidden group-hover:text-lg group-hover:inline transition-all duration-400">Jl. R.Mangun Muka Raya, RT.11/RW.14, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</p>
+        <div class="card-actions justify-end hover">
         </div>
       </div>
     </div>
-    <div class="card bg-base-100 image-full shadow-sm">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Card Title</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+    <div class="card group bg-[url(/public/backgrounds/homepage.png)] bg-cover bg-gray-500 h-90">
+      <div class="card-body rounded-lg group-hover:backdrop-blur-xs transition-all duration-400">
+        <h2 class="card-title text-7xl group-hover:text-5xl transition-all duration-400">Kampus A</h2>
+        <p class="hidden group-hover:text-2xl group-hover:inline transition-all duration-400">Kampus utama UNJ</p>
+        <p class="hidden group-hover:text-lg group-hover:inline transition-all duration-400">Jl. R.Mangun Muka Raya, RT.11/RW.14, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</p>
+        <div class="card-actions justify-end hover">
         </div>
       </div>
     </div>
-    <div class="card bg-base-100 image-full shadow-sm">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Card Title</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+    <div class="card group bg-[url(/public/backgrounds/homepage.png)] bg-cover bg-gray-500 h-90">
+      <div class="card-body rounded-lg group-hover:backdrop-blur-xs transition-all duration-400">
+        <h2 class="card-title text-7xl group-hover:text-5xl transition-all duration-400">Kampus A</h2>
+        <p class="hidden group-hover:text-2xl group-hover:inline transition-all duration-400">Kampus utama UNJ</p>
+        <p class="hidden group-hover:text-lg group-hover:inline transition-all duration-400">Jl. R.Mangun Muka Raya, RT.11/RW.14, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</p>
+        <div class="card-actions justify-end hover">
         </div>
       </div>
     </div>

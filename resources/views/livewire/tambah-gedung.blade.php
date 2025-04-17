5cf7a5204bc3faf03e15a42c09ad858bee6bf911
Acokii <zaffaz1106@gmail.com>
2025-04-18 00:01:55 +0700

    new page tambah gedung

---
+++ resources/views/livewire/tambah-gedung.blade.php
@@ -0,0 +1,26 @@
+<div class="py-5 px-40">
+    <div class="mb-10 text-4xl text-[#006569] font-bold">
+        <h3>Tambah Gedung</h3>
+    </div>
+    <div class="flex flex-col gap-4">
+        <input placeholder="Masukkan nama gedung" type="text" class="input bg-[#006569]">
+        <textarea placeholder="Masukkan deskripsi gedung" type="text" class="textarea bg-[#006569] w-full "></textarea>
+        <div class="grid grid-cols-2 gap-2">
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend text-[#006569]">Pick a file</legend>
+                <input type="file" class="file-input bg-[#006569]" />
+                <label class="fieldset-label text-[#006569]">Max size 2MB</label>
+              </fieldset>
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend text-[#006569]">Pick a file</legend>
+                <input type="file" class="file-input bg-[#006569]" />
+                <label class="fieldset-label text-[#006569]">Max size 2MB</label>
+              </fieldset>
+        </div>
+        <div class="flex gap-2">
+            <input placeholder="Masukkan Jumlah Lantai" type="number" class="input bg-[#006569]">
+            <input placeholder="Masukkan Luas Gedung" type="number" class="input bg-[#006569]">
+        </div>
+        <button class="btn btn-success">Save</button>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Livewire\Login;
 use App\Livewire\Homepage;
 use App\Livewire\Kampus;
+use App\Livewire\TambahGedung;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Homepage::class)->name('homepage');
 Route::get('/login', Login::class)->name('login');
 Route::get('/kampus', Kampus::class)->name('kampus');
+Route::get('/tambahgedung', TambahGedung::class)->name('tambah-gedung');

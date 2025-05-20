1f3fc4380969ef50cb58086ead5a8a01a5346cee
Acokii <zaffaz1106@gmail.com>
2025-05-20 09:55:56 +0700

    update dikit

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ use App\Livewire\Login;
 use App\Livewire\Homepage;
 use App\Livewire\Kampus;
 use App\Livewire\Ruang;
-use App\Livewire\TambahGedung;
+// use App\Livewire\Admin\
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Homepage::class)->name('homepage');
 Route::get('/login', Login::class)->name('login');
 Route::get('/kampus', Kampus::class)->name('kampus');
 Route::get('/gedung', Gedung::class)->name('gedung');
-Route::get('/tambahgedung', TambahGedung::class)->name('tambah-gedung');
-Route::get('/edit', EditKampus::class)->name('edit-kampus');
+// Route::get('/admin/tambahgedung', TambahGedung::class)->name('admin.tambah-gedung');
+// Route::get('/admin//edit', EditKampus::class)->name('edit-kampus');
 Route::get('/admin/dashboard', Dashboard::class)->name('admin.dashboard');
 Route::get('/ruang', Ruang::class)->name('ruang');

bcb75bb2ed04360a6a9c51b181e835effb57fdae
Acokii <zaffaz1106@gmail.com>
2025-05-20 09:57:26 +0700

    fix routing error

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
 use App\Livewire\Dashboard;
-use App\Livewire\EditKampus;
+use App\Livewire\Admin\EditKampus;
 use App\Livewire\Gedung;
 use App\Livewire\Login;
 use App\Livewire\Homepage;
 use App\Livewire\Kampus;
 use App\Livewire\Ruang;
-// use App\Livewire\Admin\
+use App\Livewire\Admin\TambahGedung;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Homepage::class)->name('homepage');
 Route::get('/login', Login::class)->name('login');
 Route::get('/kampus', Kampus::class)->name('kampus');
 Route::get('/gedung', Gedung::class)->name('gedung');
-// Route::get('/admin/tambahgedung', TambahGedung::class)->name('admin.tambah-gedung');
-// Route::get('/admin//edit', EditKampus::class)->name('edit-kampus');
+Route::get('/admin/tambahgedung', TambahGedung::class)->name('admin.tambah-gedung');
+Route::get('/admin//edit', EditKampus::class)->name('edit-kampus');
 Route::get('/admin/dashboard', Dashboard::class)->name('admin.dashboard');
 Route::get('/ruang', Ruang::class)->name('ruang');

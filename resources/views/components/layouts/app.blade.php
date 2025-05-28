d9fdf03130cec3bab2265b4c99c4da0cd086a66c
Irsyaad Wafii <wafiiirsyaad410@gmail.com>
2025-05-28 16:42:16 +0700

    ketinggalan

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,39 +11,39 @@
 
 <body class="min-h-screen bg-[url('/public/backgrounds/unj.png')] bg-cover">
     <!-- Navbar -->
-    <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200">
         <div class="px-3 py-3 lg:px-5">
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                    {{-- <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" class="p-2 text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700">
+                    {{-- <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" class="p-2 text-gray-500 rounded-lg sm:hidden hover:bg-gray-100">
                         <span class="sr-only">Open sidebar</span>
                         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z" clip-rule="evenodd"/></svg>
                     </button> --}}
                     <a href="/" class="flex items-center ms-2">
                         <img src="/logos/unj.png" class="h-12 me-3" alt="Logo">
-                        <span class="text-teal-700 text-lg font-extrabold dark:text-white leading-none">Sistem Manajemen Gedung dan Ruang<br><span class="text-teal-700 text-lg font-semibold dark:text-white leading-none">Universitas Negeri Jakarta</span></span>
+                        <span class="text-teal-700 text-lg font-extrabold leading-none">Sistem Manajemen Gedung dan Ruang<br><span class="text-teal-700 text-lg font-semibold">Universitas Negeri Jakarta</span></span>
                     </a>
                 </div>
                 <div class="flex items-center">
                     <div class="relative">
-                        <div id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                        <div id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full">
                             <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
                             </svg>
                         </div>
                         <!-- Dropdown menu -->
-                        <div id="userDropdown" class="z-10 hidden bg-gray-100 divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
-                            <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                        <div id="userDropdown" class="z-10 hidden bg-gray-100 divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+                            <div class="px-4 py-3 text-sm text-gray-900">
                                 <div>Admin</div>
                                 <div class="font-medium truncate">admin@email.com</div>
                             </div>
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
+                            <ul class="py-2 text-sm text-gray-700" aria-labelledby="avatarButton">
                                 <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
                                 </li>
                             </ul>
                             <div class="py-1">
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Logout</a>
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
                             </div>
                         </div>
                     </div>
@@ -53,20 +53,20 @@
     </nav>
 
     <!-- Sidebar -->
-    <aside id="sidebar" class="fixed top-1 left-0 z-40 w-48 h-screen pt-20 transition-transform -translate-x-full bg-teal-700 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="h-full px-3 pb-4 overflow-y-auto bg-teal-700 dark:bg-gray-800">
+    <aside id="sidebar" class="fixed top-1 left-0 z-40 w-48 h-screen pt-20 transition-transform -translate-x-full bg-teal-700 border-r border-gray-200 sm:translate-x-0">
+        <div class="h-full px-3 pb-4 overflow-y-auto bg-teal-700">
             <ul class="space-y-2">
                 <li>
-                    <a href="/" class="flex items-center p-2 rounded-lg hover:bg-teal-800 dark:hover:bg-gray-700">
-                        <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <a href="/" class="flex items-center p-2 rounded-lg hover:bg-teal-800">
+                        <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M4.857 3A1.857 1.857 0 0 0 3 4.857v4.286C3 10.169 3.831 11 4.857 11h4.286A1.857 1.857 0 0 0 11 9.143V4.857A1.857 1.857 0 0 0 9.143 3H4.857Zm10 0A1.857 1.857 0 0 0 13 4.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 9.143V4.857A1.857 1.857 0 0 0 19.143 3h-4.286Zm-10 10A1.857 1.857 0 0 0 3 14.857v4.286C3 20.169 3.831 21 4.857 21h4.286A1.857 1.857 0 0 0 11 19.143v-4.286A1.857 1.857 0 0 0 9.143 13H4.857Zm10 0A1.857 1.857 0 0 0 13 14.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 19.143v-4.286A1.857 1.857 0 0 0 19.143 13h-4.286Z" clip-rule="evenodd"/>
                         </svg>
                         <span class="text-white font-semibold ms-3">Dashboard</span>
                     </a>
                 </li>                
                 <li>
-                    <a href="/dashboard" class="flex items-center p-2 rounded-lg hover:bg-teal-800 dark:hover:bg-gray-700">
-                        <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <a href="/dashboard" class="flex items-center p-2 rounded-lg hover:bg-teal-800">
+                        <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M10.915 2.345a2 2 0 0 1 2.17 0l7 4.52A2 2 0 0 1 21 8.544V9.5a1.5 1.5 0 0 1-1.5 1.5H19v6h1a1 1 0 1 1 0 2H4a1 1 0 1 1 0-2h1v-6h-.5A1.5 1.5 0 0 1 3 9.5v-.955a2 2 0 0 1 .915-1.68l7-4.52ZM17 17v-6h-2v6h2Zm-6-6h2v6h-2v-6Zm-2 6v-6H7v6h2Z" clip-rule="evenodd"/>
                         <path d="M2 21a1 1 0 0 1 1-1h18a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1Z"/>
                         </svg>
@@ -74,8 +74,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="/content" class="flex items-center p-2 rounded-lg hover:bg-teal-800 dark:hover:bg-gray-700">
-                        <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <a href="/content" class="flex items-center p-2 rounded-lg hover:bg-teal-800">
+                        <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 6H6m12 4H6m12 4H6m12 4H6"/>
                         </svg>
                         <span class="text-white font-semibold ms-3">Daftar Isi</span>

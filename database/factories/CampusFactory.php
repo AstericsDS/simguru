f2fddc25a0c90dbd60333dbfe1b0e769538108b0
AstericsDS <andhika2312@gmail.com>
2025-06-08 18:54:39 +0700

    Add factories

---
+++ database/factories/CampusFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Campus>
+ */
+class CampusFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'admin_id' => User::factory(),
+            'address' => fake()->address(),
+            'description' => fake()->paragraph(),
+            'images_path' => fake()->image(null, 640, 480),
+            'role' => fake()->boolean()
+        ];
+    }
+}

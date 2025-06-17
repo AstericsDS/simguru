a5b1ff5579e7084701b1ee22418721b142d363ad
Acokii <zaffaz1106@gmail.com>
2025-06-17 12:09:48 +0700

    landing page and some fixes

---
+++ app/Livewire/ListKampus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ListKampus extends Component
+{
+    public function render()
+    {
+        return view('livewire.listkampus');
+    }
+}

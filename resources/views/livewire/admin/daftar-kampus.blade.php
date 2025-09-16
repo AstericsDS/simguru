94c0272c25e460a37ee932c5b3a981320e34a63c
Irsyaad Wafii <wafiiirsyaad410@gmail.com>
2025-09-16 09:06:57 +0700

    nama kampus/gedung/ruang di tabel

--- resources/views/livewire/admin/daftar-kampus.blade.php
+++ resources/views/livewire/admin/daftar-kampus.blade.php
@@ -67,7 +67,7 @@
                         {{-- Input --}}
                         <div class="grid gap-4 mb-4 grid-cols-2">
                             <div class="col-span-2">
-                                <label for="name" class="block mb-2 text-sm font-medium">Nama</label>
+                                <label for="name" class="block mb-2 text-sm font-medium">Nama Kampus</label>
                                 <input wire:model.live="name" type="text" id="name" class="bg-gray-50 border focus:outline-none focus:ring-unj transition-all text-gray-900 text-sm rounded-lg block w-full p-2.5 {{ $errors->has('name') ? 'border-red-500' : 'border-gray-300' }}">
                                 @error('name')
                                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
@@ -127,7 +127,7 @@
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3">
-                            Nama
+                            Nama Kampus
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Jumlah Gedung
